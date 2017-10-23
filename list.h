efb193584c53744eb8ed16e8571b065542171076
chenny2169 <chenny2169@gmail.com>
2017-10-23 13:46:02 +0800

    hw4 3rd ver. 1.Add the initial constructor of #list.h that I deleted in the beginning.

--- list.h
+++ list.h
@@ -8,6 +8,7 @@ using std::vector;
 
 class List : public Term {
 public:
+  List():_elements(){}
   List (vector<Term *> const & elements):_elements(elements){}
   std::string symbol() const{
     std::string ret = "[";
